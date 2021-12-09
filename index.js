2ac7286210e65ab1700e137e0ad43e9f86880cef
Rebecca Sissman <rsissman15@aol.com>
2021-12-09 12:22:56 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const element= document.getElementById("main");
+element.remove();
+
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+
+newHeader.textContent="Rebecca is the champion";
+
+document.body.append(element);
+element.append(newHeader);
